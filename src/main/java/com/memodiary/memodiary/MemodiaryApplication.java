9a44a0c1d797bd0012f49a54ecae76455297be11
Nikhil Gupta <nguptaa183@gmail.com>
2023-07-29 08:14:46 +0545

    Add files

---
+++ src/main/java/com/memodiary/memodiary/MemodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.memodiary.memodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemodiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memodiary/memodiary/MemodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memodiary.memodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
